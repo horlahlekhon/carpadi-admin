fa06aa49a9dc3c823458e580a2797e3f821f41c5
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2022-05-23 22:02:25 +0100

    Add Styled Components

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import "../styles/globals.css";
+import { ThemeProvider } from "styled-components";
+import { theme, GlobalStyles } from "../styles/theme.config";
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
---
+++ styles/theme.config.js
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from "styled-components";
+
+export const theme = {
+  primaryDarkBlue: "#243773",
+  primaryBlue: "#56A0D7",
+  primaryAshBlue: "#424F65",
+  primaryLite: "#CEDAFF",
+  primaryExtraLite: "#E7F4FB",
+  secondaryExtraBlue: "#2B55DB",
+  secondaryDeepAshBlue: "#002E45",
+  secondarySeaTone: "#51938F",
+  secondaryCyan: "#24C8D2",
+  white: "#FFFFFF",
+  black: "#000000",
+  deepGrey: "#212120",
+  grey: "#767676",
+  lightGrey: "#A3A3A3",
+  liteGrey: "#DEDEDE",
+  extraLiteGrey: "#F0F0F0",
+  ultraGrey: "#F3F3F3",
+  ultraLiteGrey: "#FCFCFC",
+  alertValidation: "#FFB82E",
+  alertValidationLite: "#FFEBA5",
+  alertError: "#F93232",
+  alertErrorLite: "#FFC1C1",
+  alertSuccess: "#439F6E",
+  alertSuccessLite: "#BCFFDB",
+};
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+    font-family: Roboto !important;
+    font-weight: 400;
+    font-style: normal;
+  }
+  input, textarea, button {font-family: inherit}
+  `;
