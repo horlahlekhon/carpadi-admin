376f0176c2b7c50b73403b764a3cbab809f4388f
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2022-06-03 21:17:38 +0100

    Collapse Sidebar on Tab View

--- src/components/layouts/core/SideNav.tsx
+++ src/components/layouts/core/SideNav.tsx
@@ -1,21 +1,34 @@
 import Image from 'next/image'
 import styles from '../../../styles/SideNav.module.css'
 import styled from 'styled-components'
-import {t} from '../../../styles/theme'
-import {Typography} from '@material-ui/core'
-import {MenuOpen} from '@material-ui/icons'
+import { t } from '../../../styles/theme'
+import { Typography } from '@material-ui/core'
+import { MenuOpen } from '@material-ui/icons'
 import Link from 'next/link'
-import {useRouter} from 'next/router'
-import {useState} from 'react'
+import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
 
 function SideNav() {
-    const router = useRouter()
-    const [isFullNav, setIsFullNav] = useState(true)
+  const router = useRouter()
+  const [isFullNav, setIsFullNav] = useState(true)
 
-    const toggleNav = () => {
-        setIsFullNav(!isFullNav)
+  const toggleNav = () => {
+    setIsFullNav(!isFullNav)
+  }
+
+  const handleResize = () => {
+    if (window.innerWidth <= 1080) {
+      setIsFullNav(false)
+    }else{
+        setIsFullNav(true)
     }
-    const Hambuger = styled.p`
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+  })
+
+  const Hambuger = styled.p`
     width: ${isFullNav ? '100%' : 'fit-content'};
     height: 48px;
     padding: 16px 25px;
@@ -24,167 +37,173 @@ function SideNav() {
     margin-bottom: 4px;
     margin-left: ${!isFullNav ? 'auto' : '0'};
     margin-right: ${!isFullNav ? 'auto' : '0'};
-    `
+  `
 
-    return (
-        <div className={isFullNav ? styles.main : styles.main__small}>
-            <div className={isFullNav ? styles.logo : styles.logo__small}>
-                <Image
-                    src={isFullNav ? '/logos/white-full.png' : '/logos/white-no-text.png'}
-                    width={isFullNav ? 244 : 56}
-                    height={isFullNav ? 93 : 56}
-                />
-            </div>
-            {isFullNav && (
-                <Pill>
-                    <Typography component={'span'}>Super Admin</Typography>
-                </Pill>
+  return (
+    <div className={isFullNav ? styles.main : styles.main__small}>
+      <div className={isFullNav ? styles.logo : styles.logo__small}>
+        <Image
+          src={isFullNav ? '/logos/white-full.png' : '/logos/white-no-text.png'}
+          width={isFullNav ? 244 : 56}
+          height={isFullNav ? 93 : 56}
+        />
+      </div>
+      {isFullNav && (
+        <Pill>
+          <Typography component={'span'}>Super Admin</Typography>
+        </Pill>
+      )}
+      <Hambuger>
+        <Image
+          src={
+            isFullNav ? '/icons/Collapse-White.svg' : '/icons/Extend-White.svg'
+          }
+          width={isFullNav ? 24 : 24}
+          height={isFullNav ? 24 : 24}
+          className={styles.pointer}
+          onClick={toggleNav}
+        />
+      </Hambuger>
+      <Nav>
+        <Link href="/">
+          <NavItem className={router.pathname == '/' ? 'active' : ''}>
+            <Image src="/icons/Home-White.svg" width={18} height={21.5} />
+            <p
+              className={`${styles.navitem__text} ${
+                isFullNav ? '' : styles.hidden
+              }`}
+            >
+              <Typography component={'span'}>Dashboard</Typography>
+            </p>
+            {router.pathname == '/' && <ActiveNavItem>&nbsp;</ActiveNavItem>}
+          </NavItem>
+        </Link>
+        <Link href="/notifications">
+          <NavItem
+            className={router.pathname == '/notifications' ? 'active' : ''}
+          >
+            <Image
+              src="/icons/Notification-White.svg"
+              width={18}
+              height={21.5}
+            />
+            <p
+              className={`${styles.navitem__text} ${
+                isFullNav ? '' : styles.hidden
+              }`}
+            >
+              <Typography component={'span'}>Notification</Typography>
+            </p>
+            {router.pathname.startsWith('/notifications') && (
+              <ActiveNavItem>&nbsp;</ActiveNavItem>
             )}
-            <Hambuger>
-                <Image src={isFullNav ? '/icons/Collapse-White.svg' : '/icons/Extend-White.svg'}
-                       width={isFullNav ? 24 : 24} height={isFullNav ? 24 : 24} className={styles.pointer}
-                       onClick={toggleNav}/>
-            </Hambuger>
-            <Nav>
-                <Link href="/">
-                    <NavItem className={router.pathname == '/' ? 'active' : ''}>
-                        <Image src="/icons/Home-White.svg" width={18} height={21.5}/>
-                        <p
-                            className={`${styles.navitem__text} ${
-                                isFullNav ? '' : styles.hidden
-                            }`}
-                        >
-                            <Typography component={'span'}>Dashboard</Typography>
-                        </p>
-                        {router.pathname == '/' && <ActiveNavItem>&nbsp;</ActiveNavItem>}
-                    </NavItem>
-                </Link>
-                <Link href="/notifications">
-                    <NavItem
-                        className={router.pathname == '/notifications' ? 'active' : ''}
-                    >
-                        <Image
-                            src="/icons/Notification-White.svg"
-                            width={18}
-                            height={21.5}
-                        />
-                        <p
-                            className={`${styles.navitem__text} ${
-                                isFullNav ? '' : styles.hidden
-                            }`}
-                        >
-                            <Typography component={'span'}>Notification</Typography>
-                        </p>
-                        {router.pathname.startsWith('/notifications') && (
-                            <ActiveNavItem>&nbsp;</ActiveNavItem>
-                        )}
-                    </NavItem>
-                </Link>
-                <Link href="/trade">
-                    <NavItem className={router.pathname == '/trade' ? 'active' : ''}>
-                        <Image src="/icons/Trade-White.svg" width={18} height={21.5}/>
-                        <p
-                            className={`${styles.navitem__text} ${
-                                isFullNav ? '' : styles.hidden
-                            }`}
-                        >
-                            <Typography component={'span'}>Trade</Typography>
-                        </p>
-                        {router.pathname.startsWith('/trade') && (
-                            <ActiveNavItem>&nbsp;</ActiveNavItem>
-                        )}
-                    </NavItem>
-                </Link>
-                <Link href="/sales">
-                    <NavItem className={router.pathname == '/sales' ? 'active' : ''}>
-                        <Image src="/icons/Sales-White.svg" width={18} height={21.5}/>
-                        <p
-                            className={`${styles.navitem__text} ${
-                                isFullNav ? '' : styles.hidden
-                            }`}
-                        >
-                            <Typography component={'span'}>Sales Platform</Typography>
-                        </p>
-                        {router.pathname.startsWith('/sales') && (
-                            <ActiveNavItem>&nbsp;</ActiveNavItem>
-                        )}
-                    </NavItem>
-                </Link>
-                <Link href="/inventory">
-                    <NavItem className={router.pathname == '/inventory' ? 'active' : ''}>
-                        <Image src="/icons/Inventory-White.svg" width={18} height={21.5}/>
-                        <p
-                            className={`${styles.navitem__text} ${
-                                isFullNav ? '' : styles.hidden
-                            }`}
-                        >
-                            <Typography component={'span'}>Inventory</Typography>
-                        </p>
-                        {router.pathname.startsWith('/inventory') && (
-                            <ActiveNavItem>&nbsp;</ActiveNavItem>
-                        )}
-                    </NavItem>
-                </Link>
-                <Link href="/users">
-                    <NavItem className={router.pathname == '/users' ? 'active' : ''}>
-                        <Image src="/icons/Users-White.svg" width={18} height={21.5}/>
-                        <p
-                            className={`${styles.navitem__text} ${
-                                isFullNav ? '' : styles.hidden
-                            }`}
-                        >
-                            <Typography component={'span'}>Users</Typography>
-                        </p>
-                        {router.pathname.startsWith('/users') && (
-                            <ActiveNavItem>&nbsp;</ActiveNavItem>
-                        )}
-                    </NavItem>
-                </Link>
-                <Link href="/account">
-                    <NavItem className={router.pathname == '/account' ? 'active' : ''}>
-                        <Image src="/icons/Account-White.svg" width={18} height={21.5}/>
-                        <p
-                            className={`${styles.navitem__text} ${
-                                isFullNav ? '' : styles.hidden
-                            }`}
-                        >
-                            <Typography component={'span'}>Account</Typography>
-                        </p>
-                        {router.pathname.startsWith('/account') && (
-                            <ActiveNavItem>&nbsp;</ActiveNavItem>
-                        )}
-                    </NavItem>
-                </Link>
-                <Link href="/settings">
-                    <NavItem className={router.pathname == '/settings' ? 'active' : ''}>
-                        <Image src="/icons/Settings-White.svg" width={18} height={21.5}/>
-                        <p
-                            className={`${styles.navitem__text} ${
-                                isFullNav ? '' : styles.hidden
-                            }`}
-                        >
-                            <Typography component={'span'}>Settings</Typography>
-                        </p>
-                        {router.pathname.startsWith('/settings') && (
-                            <ActiveNavItem>&nbsp;</ActiveNavItem>
-                        )}
-                    </NavItem>
-                </Link>
-                <Logout>
-                    <NavItem>
-                        <Image src="/icons/Logout-White.svg" width={18} height={21.5}/>
-                        <p
-                            className={`${styles.navitem__text} ${
-                                isFullNav ? '' : styles.hidden
-                            }`}
-                        >
-                            <Typography component={'span'}>Logout</Typography>
-                        </p>
-                    </NavItem>
-                </Logout>
-            </Nav>
-        </div>
-    )
+          </NavItem>
+        </Link>
+        <Link href="/trade">
+          <NavItem className={router.pathname == '/trade' ? 'active' : ''}>
+            <Image src="/icons/Trade-White.svg" width={18} height={21.5} />
+            <p
+              className={`${styles.navitem__text} ${
+                isFullNav ? '' : styles.hidden
+              }`}
+            >
+              <Typography component={'span'}>Trade</Typography>
+            </p>
+            {router.pathname.startsWith('/trade') && (
+              <ActiveNavItem>&nbsp;</ActiveNavItem>
+            )}
+          </NavItem>
+        </Link>
+        <Link href="/sales">
+          <NavItem className={router.pathname == '/sales' ? 'active' : ''}>
+            <Image src="/icons/Sales-White.svg" width={18} height={21.5} />
+            <p
+              className={`${styles.navitem__text} ${
+                isFullNav ? '' : styles.hidden
+              }`}
+            >
+              <Typography component={'span'}>Sales Platform</Typography>
+            </p>
+            {router.pathname.startsWith('/sales') && (
+              <ActiveNavItem>&nbsp;</ActiveNavItem>
+            )}
+          </NavItem>
+        </Link>
+        <Link href="/inventory">
+          <NavItem className={router.pathname == '/inventory' ? 'active' : ''}>
+            <Image src="/icons/Inventory-White.svg" width={18} height={21.5} />
+            <p
+              className={`${styles.navitem__text} ${
+                isFullNav ? '' : styles.hidden
+              }`}
+            >
+              <Typography component={'span'}>Inventory</Typography>
+            </p>
+            {router.pathname.startsWith('/inventory') && (
+              <ActiveNavItem>&nbsp;</ActiveNavItem>
+            )}
+          </NavItem>
+        </Link>
+        <Link href="/users">
+          <NavItem className={router.pathname == '/users' ? 'active' : ''}>
+            <Image src="/icons/Users-White.svg" width={18} height={21.5} />
+            <p
+              className={`${styles.navitem__text} ${
+                isFullNav ? '' : styles.hidden
+              }`}
+            >
+              <Typography component={'span'}>Users</Typography>
+            </p>
+            {router.pathname.startsWith('/users') && (
+              <ActiveNavItem>&nbsp;</ActiveNavItem>
+            )}
+          </NavItem>
+        </Link>
+        <Link href="/account">
+          <NavItem className={router.pathname == '/account' ? 'active' : ''}>
+            <Image src="/icons/Account-White.svg" width={18} height={21.5} />
+            <p
+              className={`${styles.navitem__text} ${
+                isFullNav ? '' : styles.hidden
+              }`}
+            >
+              <Typography component={'span'}>Account</Typography>
+            </p>
+            {router.pathname.startsWith('/account') && (
+              <ActiveNavItem>&nbsp;</ActiveNavItem>
+            )}
+          </NavItem>
+        </Link>
+        <Link href="/settings">
+          <NavItem className={router.pathname == '/settings' ? 'active' : ''}>
+            <Image src="/icons/Settings-White.svg" width={18} height={21.5} />
+            <p
+              className={`${styles.navitem__text} ${
+                isFullNav ? '' : styles.hidden
+              }`}
+            >
+              <Typography component={'span'}>Settings</Typography>
+            </p>
+            {router.pathname.startsWith('/settings') && (
+              <ActiveNavItem>&nbsp;</ActiveNavItem>
+            )}
+          </NavItem>
+        </Link>
+        <Logout>
+          <NavItem>
+            <Image src="/icons/Logout-White.svg" width={18} height={21.5} />
+            <p
+              className={`${styles.navitem__text} ${
+                isFullNav ? '' : styles.hidden
+              }`}
+            >
+              <Typography component={'span'}>Logout</Typography>
+            </p>
+          </NavItem>
+        </Logout>
+      </Nav>
+    </div>
+  )
 }
 
 export default SideNav
