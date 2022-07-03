086086aa1194d9edaf2fbc0b205393843131c039
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2022-07-03 20:31:17 +0100

    ft: Update

--- src/pages/inventory/archived/index.tsx
+++ src/pages/inventory/archived/index.tsx
@@ -168,7 +168,7 @@ function ArchivedInventoryPage() {
                                                 text="View"
                                                 width={66}
                                                 outlined={true}
-                                                onClick={() => handleNavigation(`/inventory/car-listings/${row.vin}`)}
+                                                onClick={() => handleNavigation(`/inventory/archived/${row.vin}`)}
                                             />
                                         </TableCell>
                                     </TableRow>
