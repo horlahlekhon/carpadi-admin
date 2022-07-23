fdacedb7ad890110cb480686abdfdeba155a7894
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2022-07-23 21:00:25 +0100

    ch: Update Config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-    reactStrictMode: true,
+    reactStrictMode: false,
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
             ? 'https://carpadi.herokuapp.com/api/v1' // development api
             : 'https://carpadi.herokuapp.com/api/v1' // production api
+    },
+    compiler: {
+        styledComponents: true,
+        ssr: false
     }
 }
