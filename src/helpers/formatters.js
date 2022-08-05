1aa4cb9b0da8e12c1c32bb943ec4b0c701766509
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2022-08-05 19:49:50 +0100

    ch: Added new util functions

--- src/helpers/formatters.js
+++ src/helpers/formatters.js
@@ -2,9 +2,13 @@ import Moment from "moment";
 
 export const formatDate = (date) => Moment(date).format('DD-MM-YYYY')
 
+export const humanReadableDate = (date) => Moment(date).format('dddd, MMMM Do YYYY')
+
 export const formatNumber = (value) => {
     if (value) {
         return Number(value).toLocaleString();
+    } else {
+        return value
     }
 }
 
