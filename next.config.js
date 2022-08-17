9d504786636779a56b5b07266fc45bde275965c5
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2022-08-17 22:38:00 +0100

    update env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     reactStrictMode: true,
     publicRuntimeConfig: {
-        cloudinaryCloudName: '',
-        cloudinaryPreset: '',
+        cloudinaryCloudName: process.env.CLOUD_NAME,
+        cloudinaryPreset: process.env.CLOUD_PRESET,
         apiUrl: process.env.NODE_ENV === 'development'
             ? 'https://carpadi.herokuapp.com/api/v1' // development api
             : 'https://carpadi.herokuapp.com/api/v1' // production api
