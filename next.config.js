6dd4cb6dc0686e96ad354872b12d967492ae6f4f
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2022-10-11 12:19:55 +0100

    breaking: Update Env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
     reactStrictMode: true,
     publicRuntimeConfig: {
+        accessKeyId: process.env.KEY_ID || '',
+        secretAccessKey: process.env.ACCESS_KEY || '',
+        region: process.env.REGION || 'eu-west-2',
+        bucket: process.env.S3_BUCKET || 'carpadi',
+        cloudfront: process.env.CLOUDFRONT_URL || 'https://d16encqm9nbktq.cloudfront.net',
         cloudinaryCloudName: process.env.CLOUD_NAME || '',
         cloudinaryPreset: process.env.CLOUD_PRESET || '',
         apiUrl: process.env.NODE_ENV === 'development'
