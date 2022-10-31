6948b714f90460fcf1323e1781377f18c5b80163
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2022-10-31 22:04:34 +0100

    ft: Add NoView to Login Page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,13 +2,13 @@ import styled from 'styled-components'
 import {t} from '../styles/theme'
 import {
     Card,
-    FormControl,
-    IconButton,
-    InputLabel,
-    Input,
-    InputAdornment,
     CardContent,
     Checkbox,
+    FormControl,
+    IconButton,
+    Input,
+    InputAdornment,
+    InputLabel,
     TextField,
     Typography
 } from '@material-ui/core'
@@ -20,7 +20,7 @@ import {useState} from 'react'
 import Button from '../components/shared/Button'
 import {authService} from '../services/auth'
 import {useRouter} from "next/router";
-import {Toaster, toast} from "react-hot-toast";
+import {toast, Toaster} from "react-hot-toast";
 
 function LoginPage() {
     const router = useRouter();
@@ -70,99 +70,108 @@ function LoginPage() {
     }
 
     return (
-        <Container>
-            <ImageBg>
-                <PhoneImage src={'/auth/phone.png'}></PhoneImage>
-            </ImageBg>
-            <FormContainer>
-                <div>
-                    <Toaster
-                        position="top-right"
-                        toastOptions={{
-                            style: {
-                                border: '1px solid #243773',
-                                padding: '16px',
-                                fontWeight: 'bold',
-                                color: '#243773'
-                            },
-                            iconTheme: {
-                                primary: '#243773',
-                                secondary: '#FFFAEE'
-                            }
-                        }}
-                    />
-                </div>
-                <div>
-                    <Typography className={classes.title}>
-                        Admin Portal
-                    </Typography>
-                    <Card
-                        variant="outlined"
-                        className={classes.card}
-                    >
-                        <CardContent className={classes.centered}>
-                            <Image
-                                src={'/logos/blue-full.png'}
-                                width="300px"
-                                height="114px"
-                            ></Image>
+        <>
+            <Container>
+                <ImageBg>
+                    <PhoneImage src={'/auth/phone.png'}></PhoneImage>
+                </ImageBg>
+                <FormContainer>
+                    <div>
+                        <Toaster
+                            position="top-right"
+                            toastOptions={{
+                                style: {
+                                    border: '1px solid #243773',
+                                    padding: '16px',
+                                    fontWeight: 'bold',
+                                    color: '#243773'
+                                },
+                                iconTheme: {
+                                    primary: '#243773',
+                                    secondary: '#FFFAEE'
+                                }
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <Typography className={classes.title}>
+                            Admin Portal
+                        </Typography>
+                        <Card
+                            variant="outlined"
+                            className={classes.card}
+                        >
+                            <CardContent className={classes.centered}>
+                                <Image
+                                    src={'/logos/blue-full.png'}
+                                    width="300px"
+                                    height="114px"
+                                ></Image>
 
-                            <TextField
-                                id="standard-basic"
-                                label="Email Address"
-                                variant="standard"
-                                type="email"
-                                fullWidth
-                                value={values.username}
-                                onChange={handleChange('username')}
-                                style={{marginTop: '20px', width: '372px'}}
-                            />
-
-                            <FormControl
-                                style={{width: '372px', marginTop: '41px'}}
-                                variant="standard"
-                            >
-                                <InputLabel htmlFor="standard-adornment-password">
-                                    Password
-                                </InputLabel>
-                                <Input
-                                    id="standard-adornment-password"
-                                    type={values.showPassword ? 'text' : 'password'}
-                                    value={values.password}
-                                    onChange={handleChange('password')}
-                                    endAdornment={
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                aria-label="toggle password visibility"
-                                                onClick={handleClickShowPassword}
-                                                onMouseDown={handleMouseDownPassword}
-                                            >
-                                                {values.showPassword ? (
-                                                    <VisibilityOff/>
-                                                ) : (
-                                                    <Visibility/>
-                                                )}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    }
+                                <TextField
+                                    id="standard-basic"
+                                    label="Email Address"
+                                    variant="standard"
+                                    type="email"
+                                    fullWidth
+                                    value={values.username}
+                                    onChange={handleChange('username')}
+                                    style={{marginTop: '20px', width: '372px'}}
                                 />
-                            </FormControl>
-                            <RememberMe>
-                                <Checkbox></Checkbox>
-                                <Typography>Remember Me</Typography>
-                            </RememberMe>
-                            <ReCAPTCHA
-                                sitekey="6LcIFiAgAAAAAO5J1kPxk306OqFVPaUUiDesLsG5"
-                                onChange={onCaptchaChange}
-                                size="normal"
-                            />
-                            <Button width="372px" marginTop="32px" text={values.isLoading ? "Loading..." : "Login"}
-                                    onClick={loginUser}/>
-                        </CardContent>
-                    </Card>
+
+                                <FormControl
+                                    style={{width: '372px', marginTop: '41px'}}
+                                    variant="standard"
+                                >
+                                    <InputLabel htmlFor="standard-adornment-password">
+                                        Password
+                                    </InputLabel>
+                                    <Input
+                                        id="standard-adornment-password"
+                                        type={values.showPassword ? 'text' : 'password'}
+                                        value={values.password}
+                                        onChange={handleChange('password')}
+                                        endAdornment={
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    aria-label="toggle password visibility"
+                                                    onClick={handleClickShowPassword}
+                                                    onMouseDown={handleMouseDownPassword}
+                                                >
+                                                    {values.showPassword ? (
+                                                        <VisibilityOff/>
+                                                    ) : (
+                                                        <Visibility/>
+                                                    )}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        }
+                                    />
+                                </FormControl>
+                                <RememberMe>
+                                    <Checkbox></Checkbox>
+                                    <Typography>Remember Me</Typography>
+                                </RememberMe>
+                                <ReCAPTCHA
+                                    sitekey="6LcIFiAgAAAAAO5J1kPxk306OqFVPaUUiDesLsG5"
+                                    onChange={onCaptchaChange}
+                                    size="normal"
+                                />
+                                <Button width="372px" marginTop="32px" text={values.isLoading ? "Loading..." : "Login"}
+                                        onClick={loginUser}/>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </FormContainer>
+            </Container>
+            <NoView>
+                <div>
+                    <Typography variant={'h5'} style={{height: 'fit-content', fontWeight: 'bold'}}>Sorry, you cannot access this app on
+                        mobile.</Typography>
+                    <Typography variant={'body1'} style={{height: 'fit-content', marginTop: '20px'}}>Try accessing it from your PC or a tablet.</Typography>
                 </div>
-            </FormContainer>
-        </Container>
+            </NoView>
+        </>
     )
 }
 
@@ -174,7 +183,26 @@ const Container = styled.div`
   background: ${t.liteGrey};
   display: flex;
   flex-direction: row;
+
+  @media only screen and (max-width: 1079px) {
+    display: none;
+  }
 `
+
+const NoView = styled.div`
+  display: none;
+  @media only screen and (max-width: 1079px) {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    text-align: center;
+    background: #243773;
+    color: whitesmoke;
+  }
+`;
 
 const PhoneImage = styled.img`
   height: 759px;
