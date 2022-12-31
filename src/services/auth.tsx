74380dba2e590bdade61de5b273a152502b0a417
Adebari Olalekan <adebari.olalekan.oluwaseun@gmail.com>
2022-12-31 16:07:39 +0100

    feat: populate car data

--- src/services/auth.tsx
+++ src/services/auth.tsx
@@ -14,7 +14,7 @@ const authSubject = new BehaviorSubject(false);
 export const authService = {
     user: userSubject.asObservable(),
     isAuthenticated: authSubject.asObservable(),
-    userValue: () => {
+    get userValue() {
         return userSubject.value
     },
     get authValue() {
