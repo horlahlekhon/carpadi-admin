8fefe5a36ca53e46b883c754016eb68ea1ef0443
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-02-19 18:31:25 +0100

    Add year filters and update graph

--- src/components/charts/LineChart.tsx
+++ src/components/charts/LineChart.tsx
@@ -1,73 +1,72 @@
-import React from 'react';
+import React from 'react'
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js';
-import {Line} from 'react-chartjs-2';
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Line } from 'react-chartjs-2'
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-);
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
 
+const LChart = ({ ttc = [], rot = [], weeks = 5 }) => {
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    elements: {
+      line: {
+        fill: false,
+        tension: 0.4
+      }
+    },
+    scales: {
+      y: {
+        display: false
+      }
+    },
+    plugins: {
+      legend: {
+        display: false
+      },
+      title: {
+        display: false
+      }
+    }
+  }
 
-const LChart = ({ttc = [], rot = []}) => {
-    const options = {
-        responsive: true,
-        maintainAspectRatio: false,
-        elements: {
-            line: {
-                fill: false,
-                tension: 0.4
-            }
-        },
-        scales: {
-            y: {
-                display: false
-            }
-        },
-        plugins: {
-            legend: {
-                display: false
-            },
-            title: {
-                display: false,
-            },
-        },
-    };
+  const labels = Array.from({ length: weeks }, (_, i) => {
+    return `WK ${i + 1}`
+  })
 
-    const labels = ['WK 1', 'WK 2', 'WK 3', 'WK 4', 'WK 5'];
-
-    const chartData = {
-        labels,
-        datasets: [
-            {
-                label: 'Total Trading Cash',
-                data: ttc,
-                borderColor: '#56A0D7',
-                backgroundColor: '#56A0D7',
-            },
-            {
-                label: 'Return On Trades',
-                data: rot,
-                borderColor: '#162A69',
-                backgroundColor: '#162A69',
-            },
-        ],
-    };
-    return (
-        <Line options={options} data={chartData} height={"136px"}/>
-    )
+  const chartData = {
+    labels,
+    datasets: [
+      {
+        label: 'Total Trading Cash',
+        data: ttc,
+        borderColor: '#56A0D7',
+        backgroundColor: '#56A0D7'
+      },
+      {
+        label: 'Return On Trades',
+        data: rot,
+        borderColor: '#162A69',
+        backgroundColor: '#162A69'
+      }
+    ]
+  }
+  return <Line options={options} data={chartData} height={'136px'} />
 }
 export default LChart
--- src/services/home.tsx
+++ src/services/home.tsx
@@ -1,22 +1,22 @@
-import {BehaviorSubject} from 'rxjs';
-import getConfig from 'next/config';
+import { BehaviorSubject } from 'rxjs'
+import getConfig from 'next/config'
 import Router from 'next/router'
-import {fetchWrapper} from '../helpers/fetchWrapper';
+import { fetchWrapper } from '../helpers/fetchWrapper'
 
-const {publicRuntimeConfig} = getConfig();
-const baseUrl = `${publicRuntimeConfig.apiUrl}/admins`;
+const { publicRuntimeConfig } = getConfig()
+const baseUrl = `${publicRuntimeConfig.apiUrl}/admins`
 
-const retrieveHomeStats = (limit = 10, offset = 1, ordering = "") => {
-    return fetchWrapper.get(`${baseUrl}/dashboards/home`)
-        .then((response) => {
-            return {status: true, data: response}
-        })
-        .catch((error) => {
-            return {status: false, data: error};
-        })
+const retrieveHomeStats = (yearOnly = true, startDate, endDate) => {
+  return fetchWrapper
+    .get(
+      `${baseUrl}/dashboards/home/?filter_year_only=${yearOnly}&start_date=${startDate}&end_date=${endDate}`
+    )
+    .then((response) => {
+      return { status: true, data: response }
+    })
+    .catch((error) => {
+      return { status: false, data: error }
+    })
 }
 
-
-export {
-    retrieveHomeStats,
-}
+export { retrieveHomeStats }
